8985372564cae22385a78ee30c1314af84ad6c27
h-youhei <hukumitu.youhei@gmail.com>
2016-11-22 15:56:17 +0900

    [zsh] show git information at prompt; close #4

--- home/.config/zsh/conf.d/prompt.zsh
+++ home/.config/zsh/conf.d/prompt.zsh
@@ -1,13 +1,37 @@
-if test -n "$SSH_CONNECTION"
+#http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
+setopt prompt_subst
+
+if test "$SSH_CONNECTION"
 then
 	local host='@%m'
 else
 	local host=''
 fi
 
-PROMPT="
-[%~]
-%n$host%#"
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' formats '(%b) '
+zstyle ':vcs_info:*' actionformats '(%b|%a) '
+
+
+#this is called before showing prompt
+precmd() {
+	vcs_info
+}
+
+_git_state() {
+	local state=`git status --porcelain -uall 2> /dev/null`
+	if test "$state"
+	then
+		echo "*"
+	else
+		echo ""
+	fi
+}
+
+PROMPT='
+$(_git_state)${vcs_info_msg_0_}[%~]
+%n$host%#'
 # while, for, etc...
 PROMPT2='%_>'
 # right
