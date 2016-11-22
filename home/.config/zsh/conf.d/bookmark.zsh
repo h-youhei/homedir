371e0b06c37f2e0587d513061574785bcdf26735
h-youhei <hukumitu.youhei@gmail.com>
2016-11-22 13:51:10 +0900

    [zsh] change bookmarks to use this repos

--- home/.config/zsh/conf.d/bookmark.zsh
+++ home/.config/zsh/conf.d/bookmark.zsh
@@ -1,4 +1,8 @@
-hash -d ws=~/workspace
-hash -d kbd=~/workspace/qmk_firmware/keyboards/ergodox/keymaps/youhei
-hash -d nv=$XDG_CONFIG_HOME/nvim
-hash -d zsh=$XDG_CONFIG_HOME/zsh
+local workspace=$HOME/workspace
+local myconfig=$workspace/myconfig
+
+hash -d ws=$workspace
+hash -d kbd=$workspace/qmk_firmware/keyboards/ergodox/keymaps/youhei
+hash -d nv=$myconfig/home/.config/nvim
+hash -d zsh=$myconfig/home/.config/zsh
+hash -d xm=$myconfig/home/.xmonad
