4b00e500efd304811a027d268c34da3c9fafef2a
h-youhei <hukumitu.youhei@gmail.com>
2016-11-24 14:46:26 +0900

    split system setup to another repos; close #9

--- init/aura-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-curl -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/aura-bin.tar.gz
-tar -xvf aura-bin.tar.gz
-cd aura-bin
-makepkg -sci
-ret=$?
-cd ..
-test $ret -eq 0 && rm -r aura-bin.tar.gz aura-bin
