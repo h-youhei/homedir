2c74b6455d2e997de3d8b66b7e94d4d99c209d48
h-youhei <hukumitu.youhei@gmail.com>
2017-02-15 11:19:32 +0900

    add backup script for fcitx

---
+++ backup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+script_path=`readlink -f $0`
+script_dir=`dirname $script_path`
+cd $script_dir
+
+fcitx_from=$XDG_CONFIG_HOME/fcitx
+
+rsync -a -v $fcitx_from/config $fcitx_from/conf $fcitx_from/addon init/fcitx
