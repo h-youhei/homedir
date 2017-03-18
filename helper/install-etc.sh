65d089ee7041bdb16d08bfc27cb055f7a5fbb476
h-youhei <hukumitu.youhei@gmail.com>
2017-03-19 02:24:05 +0900

    add mail setting

--- helper/install-etc.sh
+++ helper/install-etc.sh
@@ -3,4 +3,8 @@
 #apach web server
 systemctl enable httpd.service
 
+#screenlock after sleep
 systemctl enable xtrlock-sleep@youhei.service
+
+#mail
+systemctl enable mbsync@youhei.timer
--- home/.config/zsh/conf.d/bookmark.zsh
+++ home/.config/zsh/conf.d/bookmark.zsh
@@ -4,5 +4,6 @@ hash -d kbd=~ws/qmk_firmware/keyboards/ergodox/keymaps/youhei
 hash -d nv=~conf/home/.config/nvim
 hash -d zsh=~conf/home/.config/zsh
 hash -d xm=~conf/home/.xmonad
+hash -d mutt=~conf/home/.config/mutt
 hash -d dl=$HOME/Downloads
 hash -d snip=$XDG_CONFIG_HOME/nvim/snips
