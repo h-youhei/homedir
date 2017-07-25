h-youhei/homedir

commit fa5482fa959bb781b3807b32bd8e336b597ebe59

add flycheck, and fix warning

home/.emacs.d/config/keymap-config.el
@@ -1,0 +1,326 @@
+(require 'evil)
+(require 'ivy)
+(require 'projectile)
+(require 'autoload-init)
+;; (defvaralias 'wmap 'evil-window-map)
+;; (defvaralias 'smap 'evil-ex-search-keymap)
+;; (defvaralias 'rmap 'evil-read-key-map)
+
+(let ((nmap evil-normal-state-map)
+      (imap evil-insert-state-map)
+      (vmap evil-visual-state-map)
+      (mmap evil-motion-state-map)
+      (omap evil-operator-state-map)
+      (emap evil-emacs-state-map))
+  ;; to use as prefix key
+  (define-key mmap (kbd "SPC") nil)
+  (global-set-key (kbd "M-e") nil)
+  (global-set-key (kbd "M-a") nil)
+
+  (global-set-key [escape] #'evil-escape)
+  (define-key nmap [escape] #'evil-escape)
+
+  (global-set-key (kbd "C-u") #'ivy-resume)
+
+  ;; window
+  (global-set-key (kbd "C-t") #'open-terminal)
+  (global-set-key (kbd "M-<return>") #'make-frame-command)
+  (define-key mmap (kbd "M-SPC") #'evil-window-next)
+
+  ;; buffer
+  (define-key mmap (kbd "C-b") nil)
+  (global-set-key (kbd "C-b") #'ivy-plus-switch-buffer)
+  (global-set-key (kbd "C-S-b") #'buffer-menu)
+  (define-key mmap "q" #'evil-delete-buffer)
+  (define-key nmap "q" #'evil-delete-buffer)
+  (define-key nmap "Q" #'evil-save-and-delete-buffer)
+  (define-key nmap "+" #'save-buffer)
+
+  ;; filer
+  (define-key mmap (kbd "C-f") nil)
+  (global-set-key (kbd "C-f") #'ivy-plus-find-file)
+  (global-set-key (kbd "C-S-f") #'(lambda () (interactive) (dired "./")))
+  (define-key nmap (kbd "C-r") nil)
+  (global-set-key (kbd "C-r") #'counsel-recentf)
+  (global-set-key (kbd "C-m") #'ivy-plus-bookmark)
+  (global-set-key (kbd "C-S-m") #'bookmark-bmenu-list)
+  (define-key mmap (kbd "C-m") nil)
+  (define-key mmap [return] #'evil-ret)
+
+  (eval-when-compile (require 'dired))
+  (defvar dired-mode-map)
+  (declare-function dired-find-file "dired")
+  (let ((map dired-mode-map))
+    (define-key map [return] #'dired-find-file)
+    (define-key map (kbd "C-m") nil))
+
+  ;; search
+  (global-set-key (kbd "C-s") #'swiper)
+  (global-set-key (kbd "C-S-s") #'ivy-plus-ag-with-find-directory)
+  (global-set-key (kbd "C-i") #'counsel-imenu)
+
+  ;; start insert
+  (define-key nmap (kbd "SPC i") #'evil-insert-word)
+  (define-key nmap (kbd "SPC I") #'evil-insert-WORD)
+  (define-key nmap (kbd "SPC a") #'evil-append-word)
+  (define-key nmap (kbd "SPC A") #'evil-append-WORD)
+  (define-key nmap (kbd "SPC o") #'evil-insert-resume)
+
+  ;; start visual
+  (define-key mmap (kbd "SPC v") #'evil-visual-restore)
+  (define-key mmap (kbd "SPC g") #'mark-whole-buffer)
+  (advice-add #'mark-whole-buffer :before #'evil-set-jump)
+
+  ;; operator
+  (define-key nmap "C" #'evil-change-whole-line)
+  (define-key nmap (kbd "SPC c") #'evil-change-whole-word)
+  (define-key nmap (kbd "SPC C") #'evil-change-whole-WORD)
+  (define-key nmap "D" #'evil-delete-whole-line)
+  (define-key nmap (kbd "SPC d") #'evil-delete-whole-word)
+  (define-key nmap (kbd "SPC D") #'evil-delete-whole-WORD)
+  (define-key nmap (kbd "SPC y") #'evil-yank-whole-word)
+  (define-key nmap (kbd "SPC Y") #'evil-yank-whole-WORD)
+  (define-key nmap (kbd "SPC `") #'evil-upcase-whole-word)
+  (define-key nmap (kbd "SPC ~") #'evil-downcase-whole-word)
+  (define-key vmap "`" #'evil-upcase)
+  (define-key vmap "~" #'evil-downcase)
+  (define-key vmap "+" #'evil-rectangle-number-lines)
+
+  ;; comment
+  (define-key nmap ";" #'comment-line)
+  (define-key vmap ";" #'comment-or-uncomment-region)
+
+  ;; joint
+  (define-key nmap "j" #'evil-join-comment)
+  (define-key nmap "J" #'evil-split)
+
+  ;; delete char
+  (define-key omap (kbd "<delete>") #'evil-forward-char)
+  (define-key omap (kbd "<backspace>") #'evil-backward-char)
+  (define-key nmap (kbd "S-<delete>") #'evil-substitute)
+  (define-key nmap (kbd "<backspace>") #'evil-delete-backward-char)
+  (define-key vmap (kbd "<backspace>") #'evil-delete-backward-char)
+  (define-key nmap (kbd "S-<backspace>") #'evil-backward-substitute)
+
+  ;; undo
+  (define-key nmap "U" #'undo-tree-redo)
+  (define-key nmap (kbd "SPC u") #'undo-tree-visualize)
+
+  ;; mark
+  (define-key nmap "@" #'evil-set-marker)
+  (define-key mmap "m" #'evil-goto-mark)
+  (define-key mmap "M" #'evil-goto-mark-line)
+
+  ;; key macro
+  (define-key nmap "&" #'evil-record-macro)
+  (define-key nmap "k" #'evil-execute-macro)
+  (define-key nmap "K" #'evil-repeat-macro)
+
+  ;;command
+  (define-key nmap "!" #'shell-command)
+  (define-key vmap "!" #'evil-shell-command)
+  (define-key nmap ":" #'eval-expression)
+  (define-key nmap (kbd "SPC :") #'ielm)
+
+  ;; history
+  (define-key mmap "h" #'evil-jump-backward)
+  (define-key mmap "H" #'evil-jump-forward)
+  (define-key nmap "l" #'goto-last-change)
+  (define-key nmap "L" #'goto-last-change-reverse)
+
+  ;; search
+  (define-key mmap "#" #'evil-search-word-forward)
+  (define-key mmap "*" #'evil-search-word-backward)
+
+  ;; find
+  (define-key mmap "," #'evil-repeat-find-char)
+  (define-key mmap "<" #'evil-repeat-find-char-reverse)
+
+  ;; substitute
+  (define-key nmap "-" #'evil-ex-interactive-substitute)
+  (define-key vmap "-" #'evil-ex-interactive-substitute)
+  (define-key nmap "_" #'(lambda () (interactive) (evil-ex "%s/")))
+  (define-key nmap ">" #'evil-ex-repeat-substitute)
+
+  ;; motion
+  (define-key mmap "][" #'evil-forward-section-begin)
+  (define-key mmap "]]" #'evil-forward-section-end)
+
+  (define-key mmap (kbd "SPC e") #'evil-backward-word-end)
+  (define-key mmap (kbd "SPC E") #'evil-backward-WORD-end)
+
+  (define-key nmap "'" #'evil-use-clipboard)
+  (define-key mmap [up] #'evil-smart-previous-line)
+  (define-key imap [up] #'evil-previous-visual-line)
+  (define-key mmap [down] #'evil-smart-next-line)
+  (define-key imap [down] #'evil-next-visual-line)
+
+  (define-key mmap [home] #'evil-smart-beginning-of-line)
+  (define-key imap [home] #'evil-smart-beginning-of-line)
+  (define-key mmap [end] #'evil-smart-end-of-line)
+  (define-key imap [end] #'evil-smart-end-of-line)
+
+  ;; scroll
+  (define-key mmap [prior] #'evil-scroll-page-up)
+  (define-key imap [prior] #'evil-scroll-page-up)
+  (define-key mmap [next] #'evil-scroll-page-down)
+  (define-key imap [next] #'evil-scroll-page-down)
+  (define-key mmap (kbd "SPC <prior>") #'evil-scroll-line-to-bottom)
+  (define-key mmap (kbd "SPC <next>") #'evil-scroll-line-to-top)
+
+  ;; jump
+  (define-key mmap "g" #'evil-goto-first-line)
+  ;; get rid of predefined keys
+  (define-key nmap "g" #'evil-goto-first-line)
+  (define-key mmap "G" #'evil-smart-goto-last-line)
+  (define-key mmap (kbd "S-SPC") #'evil-middle-of-visual-line)
+  (define-key mmap (kbd "SPC SPC") #'evil-window-middle)
+  (define-key mmap (kbd "SPC <up>") #'evil-window-top)
+  (define-key mmap (kbd "SPC <down>") #'evil-window-bottom)
+  (define-key mmap (kbd "SPC <left>") #'evil-beginning-of-visual-line)
+  (define-key mmap (kbd "SPC <right>") #'evil-end-of-visual-line)
+
+  ;; indent
+  (define-key nmap [tab] #'evil-shift-right-line)
+  (define-key vmap [tab] #'evil-shift-right)
+  (define-key nmap [backtab] #'evil-shift-left-line)
+  (define-key vmap [backtab] #'evil-shift-left)
+  (define-key nmap (kbd "SPC <tab>") #'evil-align-left)
+  (define-key vmap (kbd "SPC <tab>") #'evil-align-left)
+  (define-key nmap (kbd "SPC <backtab>") #'evil-align-right)
+  (define-key vmap (kbd "SPC <backtab>") #'evil-align-right)
+  (define-key nmap "=" #'evil-indent-line)
+  (define-key nmap (kbd "SPC =") #'evil-align-center)
+  (define-key vmap (kbd "SPC =") #'evil-align-center)
+
+  ;; zap tag
+  ;; (define-key nmap "z" #')
+  ;; (define-key vmap "z" #')
+  ;; (define-key mmap "z" #')
+  ;; (define-key nmap "Z" #')
+  ;; (define-key vmap "Z" #')
+  ;; (define-key mmap "Z" #')
+
+  ;; surround
+  (define-key omap "s" #'evil-surround-edit)
+  (define-key nmap "s" #'evil-surround-region)
+  (define-key vmap "s" #'evil-surround-region)
+  (define-key nmap "S" #'evil-surround-whole-line)
+  ;; (define-key vmap "D" #'evil-surround-delete-region)
+  ;; (define-key vmap "C" #'evil-surround-change-region)
+  (define-key nmap (kbd "SPC s") #'evil-surround-whole-word)
+  (define-key nmap (kbd "SPC S") #'evil-surround-whole-WORD)
+
+  ;; exchange
+  (define-key nmap "x" #'evil-exchange)
+  (define-key vmap "x" #'evil-exchange)
+  (define-key nmap "X" #'evil-exchange-whole-line)
+  (define-key nmap (kbd "SPC x") #'evil-exchange-whole-word)
+  (define-key nmap (kbd "SPC X") #'evil-exchange-whole-WORD)
+
+  ;; flip boolean
+  (define-key nmap "`" #'bool-flip-do-flip)
+
+  ;; insert mode
+  ;; (define-key imap [tab] #'tab-to-tab-stop)
+  ;; (define-key imap [return] #'newline-and-indent)
+  ;; (defun newline-and-indent-fix ()
+  ;;   (interactive)
+  ;;   (insert "\n")
+  ;;   (back-to-indentation))
+  ;;   (indent-according-to-mode))
+  (define-key imap [return] #'evil-ret-and-indent)
+  (define-key imap (kbd "S-<return>") #'newline)
+  (define-key imap (kbd "C-b") #'evil-backward-word-begin)
+  (define-key imap (kbd "C-d") #'evil-delete-backward-word)
+  (define-key imap (kbd "C-w") #'evil-forward-word-begin)
+  ;; (define-key imap (kbd "C-u") #'evil-backward-kill-line)
+  (define-key imap (kbd "C-j") #'evil-join)
+  ;; (define-key imap (kbd "C-y") #'evil-copy-previous-line)
+  ;; (define-key imap (kbd "C-p") #'evil-copy-next-line)
+  ;;(define-key imap (kbd "C-{"))
+  ;;(define-key imap (kbd "C-}"))
+
+
+  (eval-when-compile (require 'company))
+  (defvar company-active-map)
+  (declare-function company-complete-selection "company")
+  (declare-function company-select-next "company")
+  (declare-function company-select-previous "company")
+  (let ((map company-active-map))
+    (define-key map [return] nil)
+    (define-key map [tab] #'company-complete-selection)
+    (define-key map (kbd "C-n") #'company-select-next)
+    (define-key map (kbd "C-p") #'company-select-previous)
+    (define-key map [up] nil)
+    (define-key map [down] nil))
+
+
+  (eval-when-compile (require 'yasnippet))
+  (defvar yas-minor-mode-map)
+  (defvar yas-keymap)
+  (defvar snippet-mode-map)
+  (declare-function yas-expand "yasnippet")
+  (declare-function yas-insert-snippet "yasnippet")
+  (declare-function yas-visit-snippet-file "yasnippet")
+  (declare-function yas-new-snippet "yasnippet")
+  (declare-function yas-next-field-or-maybe-expand "yasnippet")
+  (declare-function yas-prev-field "yasnippet")
+  (declare-function yas-skip-and-clear-or-delete-char "yasnippet")
+  (declare-function yas-tryout-snippet "yasnippet")
+
+  (define-key imap (kbd "C-e") #'load-yas-expand)
+  (define-key imap (kbd "C-S-e") #'load-yas-insert-snippet)
+  (global-set-key (kbd "M-e s") #'load-yas-visit-snippet-file)
+  (global-set-key (kbd "M-a s") #'load-yas-new-snippet)
+
+  (let ((map yas-minor-mode-map))
+    (evil-define-key 'insert map (kbd "C-e") #'yas-expand)
+    (evil-define-key 'insert map (kbd "C-S-e") #'yas-insert-snippet)
+    (define-key map (kbd "M-e s") #'yas-visit-snippet-file)
+    (define-key map (kbd "M-a s") #'yas-new-snippet)
+    )
+  (let ((map yas-keymap))
+    (define-key map (kbd "C-e") #'yas-next-field-or-maybe-expand)
+    (define-key map (kbd "C-S-e") #'yas-prev-field)
+    (define-key map (kbd "<delete>")
+      #'yas-skip-and-clear-or-delete-char)
+    )
+  (let ((map snippet-mode-map))
+    (define-key map (kbd "M-r") #'yas-tryout-snippet)
+    )
+
+
+  (let ((map undo-tree-visualizer-mode-map))
+    (define-key map [escape] #'undo-tree-visualizer-quit)
+    )
+
+  (let ((map minibuffer-local-map))
+    (define-key map [escape] #'minibuffer-keyboard-quit)
+    )
+
+  (let ((map ivy-minibuffer-map))
+    (define-key map [escape] #'minibuffer-keyboard-quit)
+    (define-key map [return] #'ivy-done)
+    (define-key map (kbd "S-<return>") #'ivy-immediate-done)
+    (define-key map (kbd "C-<return>") #'ivy-call)
+    (define-key map [backtab] #'ivy-dispatching-call)
+    (define-key map (kbd "C-a") #'ivy-read-action)
+    (define-key map (kbd "C-f") #'ivy-toggle-fuzzy)
+    (define-key map (kbd "C-d") #'ivy-backward-kill-word)
+    (define-key map (kbd "C-i") #'ivy-insert-current)
+    (define-key map (kbd "C-u") #'ivy-kill-line)
+    (define-key map (kbd "C-/") #'ivy-reverse-i-search)
+    )
+  (ivy-set-actions
+   t
+   '(("i" (lambda (x) (insert (if (stringp x) x (car x)))) "insert")
+     ("y" (lambda (x) (kill-new (if (stringp x) x (car x)))) "copy")))
+
+  (let ((map projectile-command-map))
+    (define-key map [escape] #'keyboard-quit)
+    (define-key map "t" #'projectile-open-terminal)
+    (define-key map "s" #'counsel-git-grep)
+    ))
+
+(provide 'keymap-config)

home/.emacs.d/config/mode-specific-config.el
@@ -1,5 +1,5 @@
-;(add-hook 'haskell-mode-hook #'haskell-mode-hooks)
 (require 'mode-local)
+(require 'evil)
 
 (with-eval-after-load 'haskell-mode
   (setq-mode-local haskell-mode
@@ -18,12 +18,16 @@
                  evil-shift-width 2
                  )
 
+(require 'bool-flip)
 (defun setup-lisp-bool-flip ()
-  (require 'bool-flip)
   (make-local-variable 'bool-flip-alist)
   (push '("t" . "nil") bool-flip-alist))
 (add-hook 'emacs-lisp-mode-hook #'setup-lisp-bool-flip)
 
+(eval-when-compile (require 'python))
 (setq python-indent-offset 4)
 
+(eval-when-compile (require 'markdown-mode))
 (setq markdown-fontify-code-blocks-natively t)
+
+(provide 'mode-specific-config)

home/.emacs.d/init.el
@@ -8,14 +8,16 @@
                               evil-exchange
                               nlinum-relative
                               save-visited-files
+                              ivy
+                              swiper
                               counsel
                               projectile
                               company
                               yasnippet
+                              flycheck
                               mmm-mode
                               bool-flip
                               haskell-mode
-                              ghc
                               company-ghc
                               markdown-mode
                               ))
@@ -23,6 +25,7 @@
 ;; appearence
 (setq inhibit-startup-screen t)
 
+(require 'whitespace)
 (setq whitespace-style '(face
                          trailing
                          tabs
@@ -48,19 +51,23 @@
 
 (setq create-lockfiles nil)
 
+(require 'recentf)
 (setq recentf-save-file (locate-user-emacs-file ".recentf")
       ;recentf-max-saved-items 30
       ;recentf-exclude '("/TAGS$" "/var/tmp/" "/tmp/")
       )
 
+(require 'savehist)
 (setq savehist-save-minibuffer-history nil
       savehist-file (locate-user-emacs-file ".history")
       savehist-additional-variables '(kill-ring
                                       evil-ex-search-history
                                       ))
 
+(require 'saveplace)
 (setq save-place-file (locate-user-emacs-file ".places"))
 
+(require 'bookmark)
 (setq bookmark-save-flag 1
       bookmark-default-file (locate-user-emacs-file ".bookmarks")
       )
@@ -95,8 +102,7 @@
 ;; use clipboard only explicitly
 (setq select-enable-clipboard nil)
 
-;; packages
-;; packape manager
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
@@ -109,7 +115,12 @@
       (package-refresh-contents)
       (mapc #'package-install not-installed))))
 
-;;evil
+(add-to-list 'load-path (locate-user-emacs-file "autoload"))
+(require 'autoload-init)
+
+;; this variable should set before loading evil
+(defvar evil-search-module 'evil-search)
+(require 'evil)
 (setq evil-cross-lines t ;at line egde
       evil-want-C-i-jump nil
       evil-want-C-d-scroll nil
@@ -117,19 +128,18 @@
       evil-text-object-change-visual-type nil
       evil-ex-substitute-case 'sensitive
       evil-ex-search-persistent-highlight nil
-      evil-search-module 'evil-search
       evil-magic 'very-nomagic
       evil-ex-visual-char-range t
       evil-ex-search-vim-style-regexp t
       evil-ex-substitute-global t
       )
 (setq-default evil-shift-width tab-width)
-(require 'evil)
 
 ;;(setq-global evil-surround-pairs-alist)
 
 (evil-declare-not-repeat #'evil-yank-line)
 
+(require 'undo-tree)
 (setq undo-tree-auto-save-history t
       undo-limit 100000
       undo-strong-limit 150000
@@ -137,8 +147,7 @@
       undo-tree-visualizer-diff t
       )
 
-(setq
-      evil-motion-state-modes (delete 'undo-tree-visualizer-mode
+(setq evil-motion-state-modes (delete 'undo-tree-visualizer-mode
                                       evil-motion-state-modes)
       evil-emacs-state-modes (append '(undo-tree-visualizer-mode
                                        dired-mode
@@ -148,49 +157,59 @@
       evil-insert-state-modes '()
       )
 
+(require 'nlinum-relative)
 ;; real number for current line
 ;; relative number for the other line
 (setq nlinum-relative-redisplay-delay 0.01)
 (add-hook 'evil-emacs-state-entry-hook #'(lambda () (nlinum-mode 0)))
 
+(require 'save-visited-files)
 (setq save-visited-files-location (locate-user-emacs-file ".visited-files")
       ;; save-visited-files-ignore-directories nil
       )
 
-;; ivy
+(require 'ivy)
 (setq ivy-wrap t
       ivy-extra-directories '()
       ivy-ignore-buffers '("\\` " "\\`\\*")
       ivy-minibuffer-faces '(ivy-minibuffer-match-face-1
                              ivy-minibuffer-match-face-2)
       )
+
+;; for ivy action that prompt me
+(setq enable-recursive-minibuffers t)
+
+(eval-when-compile (require 'swiper))
 (setq swiper-faces '(swiper-match-face-1
                      swiper-match-face-2)
       swiper-goto-start-of-match t
       swiper-action-recenter t)
 
-(setq enable-recursive-minibuffers t)
 (evil-declare-not-repeat #'ivy-plus-bookmark)
 (evil-declare-not-repeat #'ivy-plus-find-file)
-(evil-declare-not-repeat #'ivy-plus-recentf)
+(evil-declare-not-repeat #'counsel-recentf)
 (evil-declare-not-repeat #'ivy-plus-switch-buffer)
 (evil-declare-not-repeat #'ivy-plus-with-find-directory)
 (evil-declare-not-repeat #'counsel-imenu)
-(evil-declare-not-repeat #'ivy-plus-git)
+(evil-declare-not-repeat #'counsel-git)
 (evil-declare-not-repeat #'counsel-git-grep)
 (evil-declare-not-repeat #'swiper)
 (evil-declare-motion #'swiper)
 
-;; projectile
+(require 'projectile)
 (setq projectile-completion-system 'ivy
       projectile-keymap-prefix (kbd "C-p")
-      projectile-known-projects-file (locate-user-emacs-file ".projectile/bookmarks.eld")
-      projectile-cache-file (locate-user-emacs-file ".projectile/projectile.cache")
+      projectile-known-projects-file (locate-user-emacs-file
+                                      ".projectile/bookmarks.eld")
+      projectile-cache-file (locate-user-emacs-file
+                             ".projectile/projectile.cache")
       projectile-idle-timer-seconds 60
       )
 (define-key evil-normal-state-map (kbd "C-p") nil)
 
-;; company
+
+(eval-when-compile (require 'company))
+(declare-function company-abort "company")
 (setq company-idle-delay 0
       company-minimum-prefix-length 2
       company-tooltip-minimum 3
@@ -202,6 +221,10 @@
 (add-hook 'evil-insert-state-entry-hook #'(lambda () (global-company-mode 1)))
 (add-hook 'evil-insert-state-exit-hook #'company-abort)
 
+
+(eval-when-compile (require 'yasnippet))
+(declare-function yas-activate-extra-mode "yasnippet")
+(declare-function yas-abort-snippet "yasnippet")
 (setq yas-wrap-around-region t)
 ;; share fundamental-mode snippet among all modes
 (with-eval-after-load 'yasnippet
@@ -212,8 +235,7 @@
               (yas-activate-extra-mode 'fundamental-mode)))
   (add-hook 'evil-insert-state-exit-hook #'yas-abort-snippet))
 
-(add-to-list 'load-path (locate-user-emacs-file "autoload"))
-(require 'autoload-init)
+
 
 (defun evil-maybe-remove-spaces-fix (&optional do-remove)
   (if do-remove
@@ -266,6 +288,17 @@
 (mmm-add-markdown-class "emacs-lisp")
 (mmm-add-markdown-class "haskell")
 
+(require 'flycheck)
+(setq-default flycheck-emacs-lisp-load-path 'inherit
+              flycheck-emacs-lisp-initialize-packages 'auto)
+(defun flycheck-option-emacs-lisp-package-initialize-edit (orig-fun value)
+  (let ((user-init-file "~/wworkspace/myconfig/home/.emacs.d/init.el")
+        (user-emacs-directory "~/workspace/myconfig/home/.emacs.d/"))
+    (funcall orig-fun value)))
+(advice-add #'flycheck-option-emacs-lisp-package-initialize
+            :around
+            #'flycheck-option-emacs-lisp-package-initialize-edit)
+
 (ivy-mode 1)
 (evil-mode 1)
 (save-place-mode 1)
@@ -284,11 +317,16 @@
 (global-nlinum-mode 1)
 (nlinum-relative-on)
 
-(load (locate-user-emacs-file "keymap"))
-(load (locate-user-emacs-file "alias"))
-(load (locate-user-emacs-file "mode-specific"))
-(load (locate-user-emacs-file "modeline"))
-(load (locate-user-emacs-file "theme"))
+(add-to-list 'load-path (locate-user-emacs-file "config"))
+(require 'keymap-config)
+(require 'alias-config)
+(require 'mode-specific-config)
+(require 'modeline-config)
+(require 'theme-config)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
 
 (setq custom-file (locate-user-emacs-file ".custom.el"))
 (load custom-file)
+
+(provide 'init)

home/.emacs.d/.dir-locals.el
@@ -1,0 +1,1 @@
+((emacs-lisp-mode . ((flycheck-disabled-checkers . (emacs-lisp-checkdoc)))))

home/.emacs.d/alias.el
@@ -1,2 +1,0 @@
-(defalias 'rldc 'reload-config)
-(defalias 'pd 'package-delete)

home/.emacs.d/config/alias-config.el
@@ -1,0 +1,13 @@
+(require 'autoload-init)
+
+(defalias 'rc #'reload-config)
+
+(defalias 'pd #'package-delete)
+(defalias 'pl #'package-list-packages)
+(defalias 'par #'package-autoremove)
+
+(require 'flycheck)
+;;; Error List
+(defalias 'el #'flycheck-list-errors)
+
+(provide 'alias-config)

home/.emacs.d/config/modeline-config.el
@@ -26,6 +26,7 @@
     ;; (format "%s%80s" left right)
     (format (format "%%s%%%ds" available-width) left right)))
 
+(require 'nlinum)
 (defun window-total-width-workaround ()
   ;; 2 is fringe
   (+ (window-width) 2 nlinum--width))
@@ -42,6 +43,7 @@
   (let ((name (buffer-name)))
     (substring name 0 (min (length name) max-length))))
 
+(require 'projectile)
 (defvar-local mode-line-current-project-and-directory-cache nil)
 (put 'mode-line-current-project-and-directory-cache 'permanent-local t)
 (defun mode-line-current-project-and-directory ()
@@ -73,6 +75,7 @@
           (t
            (concat " [" enc "]")))))
 
+(require 'evil)
 (defun mode-line-evil-state ()
   (let ((s
          (cond ((evil-normal-state-p)
@@ -127,3 +130,5 @@
         (setq output (concat dir "/" output)
               hierarchy (1- hierarchy))))
     (concat head output)))
+
+(provide 'modeline-config)

home/.emacs.d/config/theme-config.el
@@ -65,3 +65,5 @@
                       :underline t)
   (set-face-attribute 'whitespace-space nil
                       :background "LightSkyBlue"))
+
+(provide 'theme-config)

commit 163636ade579d972d0a79ccf127dbbaf3f0e38f9

re-enable on-demand loading. caused by fa5482f

home/.emacs.d/config/keymap-config.el
@@ -47,12 +47,11 @@
   (define-key mmap (kbd "C-m") nil)
   (define-key mmap [return] #'evil-ret)
 
-  (eval-when-compile (require 'dired))
-  (defvar dired-mode-map)
   (declare-function dired-find-file "dired")
-  (let ((map dired-mode-map))
-    (define-key map [return] #'dired-find-file)
-    (define-key map (kbd "C-m") nil))
+  (with-eval-after-load 'dired
+    (let ((map dired-mode-map))
+      (define-key map [return] #'dired-find-file)
+      (define-key map (kbd "C-m") nil)))
 
   ;; search
   (global-set-key (kbd "C-s") #'swiper)
@@ -242,54 +241,50 @@
   ;;(define-key imap (kbd "C-}"))
 
 
-  (eval-when-compile (require 'company))
-  (defvar company-active-map)
-  (declare-function company-complete-selection "company")
-  (declare-function company-select-next "company")
-  (declare-function company-select-previous "company")
-  (let ((map company-active-map))
-    (define-key map [return] nil)
-    (define-key map [tab] #'company-complete-selection)
-    (define-key map (kbd "C-n") #'company-select-next)
-    (define-key map (kbd "C-p") #'company-select-previous)
-    (define-key map [up] nil)
-    (define-key map [down] nil))
+  (with-eval-after-load 'company
+    (declare-function company-complete-selection 'company)
+    (declare-function company-select-next 'company)
+    (declare-function company-select-previous 'company)
+    (let ((map company-active-map))
+      (define-key map [return] nil)
+      (define-key map [tab] #'company-complete-selection)
+      (define-key map (kbd "C-n") #'company-select-next)
+      (define-key map (kbd "C-p") #'company-select-previous)
+      (define-key map [up] nil)
+      (define-key map [down] nil)))
 
 
-  (eval-when-compile (require 'yasnippet))
-  (defvar yas-minor-mode-map)
-  (defvar yas-keymap)
-  (defvar snippet-mode-map)
-  (declare-function yas-expand "yasnippet")
-  (declare-function yas-insert-snippet "yasnippet")
-  (declare-function yas-visit-snippet-file "yasnippet")
-  (declare-function yas-new-snippet "yasnippet")
-  (declare-function yas-next-field-or-maybe-expand "yasnippet")
-  (declare-function yas-prev-field "yasnippet")
-  (declare-function yas-skip-and-clear-or-delete-char "yasnippet")
-  (declare-function yas-tryout-snippet "yasnippet")
+  (with-eval-after-load 'yassnippet
+    (declare-function yas-expand 'yasnippet)
+    (declare-function yas-insert-snippet 'yasnippet)
+    (declare-function yas-visit-snippet-file 'yasnippet)
+    (declare-function yas-new-snippet 'yasnippet)
+    (declare-function yas-next-field-or-maybe-expand 'yasnippet)
+    (declare-function yas-prev-field 'yasnippet)
+    (declare-function yas-skip-and-clear-or-delete-char 'yasnippet)
+    (declare-function yas-tryout-snippet 'yasnippet)
 
-  (define-key imap (kbd "C-e") #'load-yas-expand)
-  (define-key imap (kbd "C-S-e") #'load-yas-insert-snippet)
-  (global-set-key (kbd "M-e s") #'load-yas-visit-snippet-file)
-  (global-set-key (kbd "M-a s") #'load-yas-new-snippet)
+    (define-key imap (kbd "C-e") #'load-yas-expand)
+    (define-key imap (kbd "C-S-e") #'load-yas-insert-snippet)
+    (global-set-key (kbd "M-e s") #'load-yas-visit-snippet-file)
+    (global-set-key (kbd "M-a s") #'load-yas-new-snippet)
 
-  (let ((map yas-minor-mode-map))
-    (evil-define-key 'insert map (kbd "C-e") #'yas-expand)
-    (evil-define-key 'insert map (kbd "C-S-e") #'yas-insert-snippet)
-    (define-key map (kbd "M-e s") #'yas-visit-snippet-file)
-    (define-key map (kbd "M-a s") #'yas-new-snippet)
+    (let ((map yas-minor-mode-map))
+      (evil-define-key 'insert map (kbd "C-e") #'yas-expand)
+      (evil-define-key 'insert map (kbd "C-S-e") #'yas-insert-snippet)
+      (define-key map (kbd "M-e s") #'yas-visit-snippet-file)
+      (define-key map (kbd "M-a s") #'yas-new-snippet)
+      )
+    (let ((map yas-keymap))
+      (define-key map (kbd "C-e") #'yas-next-field-or-maybe-expand)
+      (define-key map (kbd "C-S-e") #'yas-prev-field)
+      (define-key map (kbd "<delete>")
+        #'yas-skip-and-clear-or-delete-char)
+      )
+    (let ((map snippet-mode-map))
+      (define-key map (kbd "M-r") #'yas-tryout-snippet)
+      )
     )
-  (let ((map yas-keymap))
-    (define-key map (kbd "C-e") #'yas-next-field-or-maybe-expand)
-    (define-key map (kbd "C-S-e") #'yas-prev-field)
-    (define-key map (kbd "<delete>")
-      #'yas-skip-and-clear-or-delete-char)
-    )
-  (let ((map snippet-mode-map))
-    (define-key map (kbd "M-r") #'yas-tryout-snippet)
-    )
-
 
   (let ((map undo-tree-visualizer-mode-map))
     (define-key map [escape] #'undo-tree-visualizer-quit)

home/.emacs.d/config/mode-specific-config.el
@@ -17,17 +17,14 @@
                  tab-width 2
                  evil-shift-width 2
                  )
-
 (require 'bool-flip)
 (defun setup-lisp-bool-flip ()
   (make-local-variable 'bool-flip-alist)
   (push '("t" . "nil") bool-flip-alist))
 (add-hook 'emacs-lisp-mode-hook #'setup-lisp-bool-flip)
 
-(eval-when-compile (require 'python))
 (setq python-indent-offset 4)
 
-(eval-when-compile (require 'markdown-mode))
 (setq markdown-fontify-code-blocks-natively t)
 
 (provide 'mode-specific-config)

home/.emacs.d/init.el
@@ -178,7 +178,6 @@
 ;; for ivy action that prompt me
 (setq enable-recursive-minibuffers t)
 
-(eval-when-compile (require 'swiper))
 (setq swiper-faces '(swiper-match-face-1
                      swiper-match-face-2)
       swiper-goto-start-of-match t
@@ -207,8 +206,7 @@
 (define-key evil-normal-state-map (kbd "C-p") nil)
 
 
-(eval-when-compile (require 'company))
-(declare-function company-abort "company")
+(declare-function company-abort 'company)
 (setq company-idle-delay 0
       company-minimum-prefix-length 2
       company-tooltip-minimum 3
@@ -221,9 +219,8 @@
 (add-hook 'evil-insert-state-exit-hook #'company-abort)
 
 
-(eval-when-compile (require 'yasnippet))
-(declare-function yas-activate-extra-mode "yasnippet")
-(declare-function yas-abort-snippet "yasnippet")
+(declare-function yas-activate-extra-mode 'yasnippet)
+(declare-function yas-abort-snippet 'yasnippet)
 (setq yas-wrap-around-region t)
 ;; share fundamental-mode snippet among all modes
 (with-eval-after-load 'yasnippet
@@ -299,8 +296,7 @@
             #'flycheck-option-emacs-lisp-package-initialize-edit)
 
 
-(eval-when-compile (require 'magit))
-(declare-function magit-restore-window-configuration "magit-mode")
+(declare-function magit-restore-window-configuration 'magit-mode)
 ;; kill buffer when I quit from magit
 (setq magit-bury-buffer-function #'(lambda (_)
                                      (magit-restore-window-configuration t))