7dae48615968b9e75b0131db413f0776ef6f134a
h-youhei <hukumitu.youhei@gmail.com>
2018-01-11 23:05:30 +0900

    [emacs] fix warning

--- home/.emacs.d/autoload/ivy-plus-autoload.el
+++ home/.emacs.d/autoload/ivy-plus-autoload.el
@@ -2,6 +2,7 @@
 (autoload #'ivy-plus-find-file "ivy-plus" "" t)
 (autoload #'ivy-plus-recentf "ivy-plus" "" t)
 (autoload #'ivy-plus-bookmark "ivy-plus" "" t)
+(autoload #'ivy-plus-with-find-directory "ivy-plus" "" t)
 (autoload #'ivy-plus-with-completing-directory "ivy-plus" "" t)
 
 (provide 'ivy-plus-autoload)
