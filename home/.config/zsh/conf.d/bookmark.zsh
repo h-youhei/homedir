70ed171867ac389900a858eb2b192a418efd3a9f
h-youhei <hukumitu.youhei@gmail.com>
2018-01-11 15:55:04 +0900

    [zsh]change bookmark adress for keyboard

--- home/.config/zsh/conf.d/bookmark.zsh
+++ home/.config/zsh/conf.d/bookmark.zsh
@@ -1,6 +1,6 @@
 hash -d ws=$HOME/workspace
 hash -d conf=~ws/myconfig
-hash -d kbd=~ws/qmk_firmware/keyboards/ergodox/keymaps/youhei
+hash -d kbd=~ws/qmk_firmware/layouts/ergodox/keymaps/youhei
 hash -d nv=~conf/home/.config/nvim
 hash -d zsh=~conf/home/.config/zsh
 hash -d xm=~conf/home/.xmonad
