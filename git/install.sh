0f11a231e74e488b141c25ca031626989bbac35c
h-youhei <hukumitu.youhei@gmail.com>
2018-02-28 15:51:05 +0900

    make install script simple because git config accepts symlink

--- git/install.sh
+++ git/install.sh
@@ -4,8 +4,9 @@ script_path=`readlink -f $0`
 script_dir=`dirname $script_path`
 
 [ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME=$HOME/.config
-[ -d $XDG_CONFIG_HOME/git ] mkdir -p $XDG_CONFIG_HOME/git
-[ -f $XDG_CONFIG_HOME/git/config ] cp -a $script_dir/config $XDG_CONFIG_HOME/git/.
+git_dir=$XDG_CONFIG_HOME/git
+[ -d $git_dir ] || mkdir -p git_dir
+ln -s $script_dir/config $git_dir/.
 
 cd $script_dir
-git remote set-url origin git@github.com:h-youhei/homedir.git
+#git remote set-url origin git@github.com:h-youhei/homedir.git
