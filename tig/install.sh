edb90f64f8997c030712475675740cafa82aef78
h-youhei <hukumitu.youhei@gmail.com>
2018-03-20 11:20:12 +0900

    add tig setting

---
+++ tig/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+script_path=`readlink -f $0`
+script_dir=`dirname $script_path`
+
+[ -z $XDG_CONFIG_HOME ] && XDG_CONFIG_HOME=$HOME/.config
+tig_dir=$XDG_CONFIG_HOME/tig
+[ -d $tig_dir ] || mkdir -p $tig_dir
+ln -s $script_dir/config $tig_dir/
+
+[ -z $XDG_DATA_HOME ] && XDG_DATA_HOME=$HOME/.local/share
+[ -d $XDG_DATA_HOME/tig ] || mkdir -p $XDG_DATA_HOME/tig
