25bae62ebe66385cd546c68ec9b948be2c5053b6
h-youhei <hukumitu.youhei@gmail.com>
2018-03-21 17:44:47 +0900

    update install script

--- install.sh
+++ install.sh
@@ -4,23 +4,28 @@ script_path=`readlink -f $0`
 script_dir=`dirname $script_path`
 cd $script_dir
 
+# order is consern
 xdg/install.sh
 sh-common/install.sh
 bash/install.sh
 zsh/install.sh
 systemd/install.sh
+X11/install.sh 
+
+
+ripgrep/install.sh
 tmux/install.sh
 vim/install.sh
-X11/install.sh 
 fontconfig/install.sh
 xmonad/install.sh
 stalontray/install.sh
-xterm/install.sh
+mlterm/install.sh
 fcitx/install.sh
 mozc/install.sh
+tig/install.sh
 pacman/install.sh
 
 [ -d $HOME/bin ] || mkdir $HOME/bin
 bin() {
-	ln -s "$script_dir/bin/$1" $HOME/bin/.
+	ln -s "$script_dir/bin/$1" $HOME/bin/
 }
