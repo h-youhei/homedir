7bb920efd36885d12859c6f6d4d93b2104be84ae
h-youhei <hukumitu.youhei@gmail.com>
2018-03-21 17:47:34 +0900

    move to h-youhei/rootdir related #41

--- helper/install-home.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-xmonad_mark=$HOME/.xmonad/mark
-test ! -f "$xmonad_mark" && echo $HOME > $xmonad_mark
-
-systemctl --user enable ssh-agent.service
--- helper/update-etc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-script_path=`readlink -f $0`
-script_dir=`dirname $script_path`
-cd $script_dir
-
-rsync -rlpt -v --exclude-from=exclude-etc ../etc/ /etc
