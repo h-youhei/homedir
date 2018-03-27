4aba3e70b6afd06eaadeb63573190daa95651c4a
Hukumitu Youhei <hukumitu.youhei@gmail.com>
2018-03-27 11:48:01 +0900

    match keybind between zsh and readline

--- zsh/keybind.zsh
+++ zsh/keybind.zsh
@@ -14,24 +14,6 @@ unset f
 
 KEYTIMEOUT=10
 
-function capitalize-whole-word () {
-	zle backward-word
-	zle capitalize-word
-}
-zle -N capitalize-whole-word
-
-function upcase-whole-word () {
-	zle backward-word
-	zle up-case-word
-}
-zle -N upcase-whole-word
-
-function downcase-whole-word () {
-	zle backward-word
-	zle down-case-word
-}
-zle -N downcase-whole-word
-
 function insert-to-visual () {
 	zle vi-cmd-mode
 	zle visual-mode
@@ -72,26 +54,27 @@ bindkey '\b' backward-delete-char
 bindkey "${key[Delete]}" delete-char
 bindkey "${key[Insert]}" overwrite-mode
 bindkey '\eb' backward-word
-bindkey '\ec' capitalize-whole-word
-bindkey '\ed' backward-kill-word
-bindkey '\eD' backward-kill-line
-bindkey '\ef' vi-find-next-char
-bindkey '\eF' vi-find-prev-char
-bindkey '\eh' upcase-whole-word
+bindkey '\ed' kill-word
+#kill to End
+bindkey '\ee' kill-line
+bindkey '\ef' forward-word
+#kill to Home
+bindkey '\eh' backward-kill-line
 bindkey '\ei' insert-last-word
-bindkey '\el' downcase-whole-word
+bindkey '\el' kill-whole-line
 bindkey '\ep' yank
 bindkey '\eP' get-line
-bindkey '\er' transpose-words
-bindkey '\et' vi-find-next-char-skip
-bindkey '\eT' vi-find-prev-char-skip
+bindkey '\et' transpose-char
+bindkey '\eT' transpose-words
 bindkey '\eu' undo
 bindkey '\eU' redo
 bindkey '\ev' insert-to-visual
 bindkey '\eV' insert-to-visual-line
-bindkey '\ew' forward-word
+bindkey '\ew' backward-kill-word
 bindkey '\ey' copy-prev-word
 bindkey '\eY' push-line
+bindkey '\e]' vi-find-next-char
+bindkey '\e[' vi-find-prev-char-skip
 bindkey '\e~' vi-swap-case
 bindkey '\e,' vi-repeat-find
 bindkey '\e<' vi-rev-repeat-find
@@ -99,7 +82,7 @@ bindkey "\e'" vi-set-buffer
 bindkey '\e%' vi-match-bracket
 bindkey '\e:' execute-named-cmd
 bindkey '\e^' cd-parent
-bindkey '\e' kill-whole-line
+bindkey '\e' send-break
 
 #visual
 bindkey -M visual "${key[Down]}" down-line
@@ -112,15 +95,7 @@ bindkey -M visual '\b' vi-delete
 bindkey -M visual "${key[Delete]}" vi-delete
 
 bindkey -M visual 'b' backward-word
-bindkey -M visual 'c' vi-change
-bindkey -M visual 'd' vi-delete
-bindkey -M visual 'f' vi-find-char
-bindkey -M visual 'F' vi-find-prev-char
-bindkey -M visual 't' vi-find-next-char-skip
-bindkey -M visual 'T' vi-find-prev-char-skip
 bindkey -M visual 'w' forward-word
-bindkey -M visual 'W' vi-forward-blank-word-end
-bindkey -M visual 'y' vi-yank
 bindkey -M visual '`' vi-uppercase
 bindkey -M visual '~' vi-lowercase
 bindkey -M visual '%' vi-match-bracket
@@ -128,15 +103,3 @@ bindkey -M visual ',' vi-repeat-find
 bindkey -M visual '<' vi-rev-repeat-find
 
 bindkey -M visual '\e' visual-to-insert
-#
-#digit-argument
-
-#Text objects
-#BUG?: no such keymap 'visual'
-#bindkey -M visua1 'aw' select-a-word
-#bindkey -M visua1 'iw' select-in-word
-#bindkey -M visua1 'aW' select-a-blank-word
-#bindkey -M visua1 'iW' select-in-blank-word
-#argument
-#bindkey -M visua1 'aa' select-a-shell-word
-#bindkey -M visua1 'ia' select-in-shell-word
